40851067bd1ba7facc81031c3568efa18d4fd553
Brendan Bonner <brendan.bonner2@mail.dcu.ie>
2021-03-16 16:11:21 +0000

    slow down grid

--- src/4-visualise-metrics/quick3js/js/gridDisplay.js
+++ src/4-visualise-metrics/quick3js/js/gridDisplay.js
@@ -6,34 +6,47 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 
-const controls = new OrbitControls(camera, canvas);
+/*const controls = new OrbitControls(camera, canvas);
 controls.target.set(0, 5, 0);
 controls.update();
+*/
 
 var MAX_LEN = 25;
 var cubes = [];
 var x, y, z;
 var geometry = new THREE.BoxGeometry(0.001, 0.001, 0.001);
 var geometry1 = new THREE.BoxGeometry(0.05, 0.05, 0.05);
-var geometry2 = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+var geometry2 = new THREE.BoxGeometry(0.2, 0.1, 0.1);
 
 var geometry_arr = [geometry, geometry1, geometry2];
 
 var material = new THREE.MeshBasicMaterial({
-  color: 0x0022aa
+  color: 0xaaaa00
+});
+var materialDense = new THREE.MeshBasicMaterial({
+  color: 0x00aaaa
+});
+var materialOutput = new THREE.MeshBasicMaterial({
+  color: 0xaa00aa
 });
 
-var LAYERS = 50;
+var LAYERS = 40;
 
-camera.position.x = MAX_LEN / 2;
-camera.position.y = MAX_LEN / 2;
-camera.position.z = 75;
+camera.position.x = MAX_LEN / 3;
+camera.position.y = MAX_LEN / 3;
+camera.position.z = 90;
 for (z = 0; z < LAYERS; z++) {
   var layer_distance = Math.random();
   for (x = 0; x < MAX_LEN; x++) {
     for (y = 0; y < MAX_LEN; y++) {
-      number = (z * (x * MAX_LEN) + y); 
-      cube = new THREE.Mesh(geometry, material);
+      number = (z * (x * MAX_LEN) + y);
+      if(z >= LAYERS - 2){
+        cube = new THREE.Mesh(geometry, materialOutput);
+      } else if (z > 10){
+        cube = new THREE.Mesh(geometry, materialDense);
+      } else {
+        cube = new THREE.Mesh(geometry, material);
+      }
       cubes.push(cube);
       scene.add(cubes[number]);
       cubes[number].position.x = x; 
@@ -44,7 +57,7 @@ for (z = 0; z < LAYERS; z++) {
   }
 
   var change = 0.1;
-  camera.lookAt(MAX_LEN / 2, 0, 0);
+  camera.lookAt(MAX_LEN / 3, 0, 0);
 
 
 
@@ -63,8 +76,8 @@ for (z = 0; z < LAYERS; z++) {
         }
       }
     }
-    if (camera.position.x > 10) change = -0.1;
-    if (camera.position.x < -10) change = 0.1;
+    if (camera.position.x > 20) change = -0.1;
+    if (camera.position.x < -20) change = 0.1;
     camera.position.x = camera.position.x + change;
 
 
