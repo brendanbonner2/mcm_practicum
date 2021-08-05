2e0658197884b49d45faf514ef60290737b157e3
Brendan Bonner <brendanboner@gmail.com>
2021-08-06 00:16:20 +0100

    Update Package Manager

--- src/package/setup.py
+++ src/package/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lifecycle-pkg-bonnerb2",
+    name="lifecycle",
     version="0.0.1",
     author="Brendan Bonner",
     author_email="brendan.bonner2@mail.dcu.ie",
@@ -23,4 +23,7 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+        install_requires=[
+        'DeepDiff',
+        'pymongo',
 )
